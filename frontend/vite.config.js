640456be9250a9fe67ca2c1fa90d4c2980b9cc7f
Yash Kulkarni <110209883+yaasshhh20@users.noreply.github.com>
2025-01-24 19:54:16 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    assetsInclude: ['**/*.PNG', '**/*.png'], // Add this line
+    assetsInclude: ['**/*.PNG', '**/*.png'], // Add this line to the code
 });
